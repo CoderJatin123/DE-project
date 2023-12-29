48ab09d1ad4ce684fcb61c09da422594d8bd09a0
Jatin <coderjatin123@gmail.com>
2023-12-29 21:03:19 +0530

    initial commit

---
+++ test.py
@@ -0,0 +1,14 @@
+import tkinter
+import tkintermapview
+
+# create tkinter window
+root_tk = tkinter.Tk()
+root_tk.geometry(f"{1000}x{700}")
+root_tk.title("Smart Traffic management")
+
+# create map widget
+map_widget = tkintermapview.TkinterMapView(root_tk, width=1000, height=700, corner_radius=0)
+map_widget.pack(fill="both", expand=True)
+map_widget.set_address("Gujarat")
+
+root_tk.mainloop()
